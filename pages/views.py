4e84316781d03d9a3089a87ba5bf33c4fbce671b
Musthak <musthakcool55@gmail.com>
2026-06-14 19:20:04 +0530

    Link home page to hotels app
    - HomePageView now passes featured_hotels queryset to template
    - Hero search form submits to hotels:hotel_list with q param
    - Popular hotels section renders real Hotel objects with links to detail pages
    - CTA button links to hotel list instead of signup
    - View all link added next to section heading

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
+from hotels.models import Hotel
 
-# Create your views here.
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["featured_hotels"] = Hotel.objects.filter(
+            is_active=True
+        ).order_by("-created_at")[:6]
+        return context
