0a2f1e2e5034f77fc47f5e7f5dd2c7455e94c35a
Musthak <musthakcool55@gmail.com>
2026-06-16 11:12:09 +0530

    Add /health/ endpoint and update Railway healthcheck path

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,8 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+def healthcheck(request):
+    return HttpResponse("ok")
 
 urlpatterns = [
+    path("health/", healthcheck, name="healthcheck"),
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
